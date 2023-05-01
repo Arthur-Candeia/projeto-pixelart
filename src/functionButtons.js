994cdde86b2b45886744cac579125ea12a0ed815
Arthur-Candeia <arthurheher@hotmail.com>
2023-05-01 03:06:54 -0300

    mudanca-qtdPixels

--- src/functionButtons.js
+++ src/functionButtons.js
@@ -1,14 +1,8 @@
-export {functionX4, functionX9, functionX15, functionClear, functionEraser}
+export {functionX9, functionX30, functionX60, functionClear, functionEraser}
 
 const pixels = document.getElementById('pixels')
 const inputColor = document.getElementById('inputColor')
 let verifyEraser = 1
-
-const functionX4 = () => {
-  pixels.innerHTML = ''
-  pixels.style.width = '160px'
-  resize(16, 40) //qtdPixels, pixelSize
-}
 
 const functionX9 = () => {
   pixels.innerHTML = ''
@@ -16,15 +10,23 @@ const functionX9 = () => {
   resize(81, 35) //qtdPixels, pixelSize
 }
 
-const functionX15 = () => {
+const functionX30 = () => {
   pixels.innerHTML = ''
   pixels.style.width = '450px'
-  resize(225, 30) //qtdPixels, pixelSize
+  resize(900, 15) //qtdPixels, pixelSize
+}
+
+const functionX60 = () => {
+  pixels.innerHTML = ''
+  pixels.style.width = '600px'
+  resize(3600, 10) //qtdPixels, pixelSize
 }
 
 const resize = (qtdPixels, pixelSize) => { //funcao que muda o tamanho
-  for (let i = 0; i < qtdPixels; i++) {
-    pixels.innerHTML += '<div class="pixel"></div>'
+  for (let i = 0; i < qtdPixels; ++i) {
+    let div = document.createElement('div') //APRENDIZADO 1
+    div.className = 'pixel'
+    pixels.append(div)
     document.querySelectorAll('.pixel')[i].style.width = `${pixelSize}px`
     document.querySelectorAll('.pixel')[i].style.height = `${pixelSize}px`
   }
@@ -50,3 +52,10 @@ const functionEraser = () => { //borracha
     })
   })
 }
+
+/*
+APRENDIZADO 1:
+Usar o método appendChild() em um loop é geralmente mais eficiente do que usar a propriedade innerHTML em JavaScript, especialmente quando você está adicionando muitos elementos ao DOM. Isso ocorre porque a propriedade innerHTML substitui o conteúdo existente de um elemento HTML e analisa o novo conteúdo, o que pode ser um processo demorado quando muitos elementos são adicionados.
+
+Por outro lado, o método appendChild() adiciona um novo nó de elemento ao final do elemento pai, sem afetar o conteúdo existente. Como resultado, é mais rápido e eficiente em termos de uso de recursos.
+*/
--- src/script.js
+++ src/script.js
@@ -1,18 +1,18 @@
-import { functionX4, functionX9, functionX15, functionClear, functionEraser } from "./functionButtons.js"
+import { functionX9, functionX30, functionX60, functionClear, functionEraser } from "./functionButtons.js"
 
-const buttonX4 = document.getElementById('x4')
 const buttonX9 = document.getElementById('x9')
-const buttonX15 = document.getElementById('x15')
+const buttonX30 = document.getElementById('x30')
+const buttonX60 = document.getElementById('x60')
 const clearButton = document.getElementById('clearButton')
 const eraserButton = document.getElementById('eraserButton')
 const inputColor = document.getElementById('inputColor')
 
-functionX4()
-buttonX4.addEventListener('click', functionX4)
-
+functionX9()
 buttonX9.addEventListener('click', functionX9)
 
-buttonX15.addEventListener('click', functionX15)
+buttonX30.addEventListener('click', functionX30)
+
+buttonX60.addEventListener('click', functionX60)
 
 clearButton.addEventListener('click', functionClear)
 
